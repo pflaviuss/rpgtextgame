7df1e13d3728ba5804faf9483e27693a25fcf6cc
Flavius <flavius.popa123@gmail.com>
2022-11-25 13:04:40 +0200

    added the win game functions, breaking the weapon functions, and the final display, at this point the games goes pretty fine but still have to work a little bit on the end part of the game

--- script.js
+++ script.js
@@ -61,7 +61,18 @@ const locations = [
     ],
     "button functions": [restart, restart, restart],
     text: "You die. 💀",
+  },
+  {
+    name:"win",
+    "button text": [
+      "Replay?",
+      "Replay?",
+      "Replay?"
+    ],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! You win the game! ✌️ ",
   }
+  
 ]; // an array that has a object
 
 const button1 = document.querySelector("#button1");
@@ -210,16 +221,39 @@ function goFight(){
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText = " You attack it with your " + weapons[currentWeapon].name + ".";
-  health -= monsters[fighting].level;
+  if(isMonsterHit()){
+    health -= getMonsterAttackValue(monsters[fighting].level);
+  } else {
+    text.innerText += " You miss.";
+  }
+  health -= getMonsterAttackValue(monsters[fighting].level);
   monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0){
     lose();
   }else if (monsterHealth <= 0){
-    defeatMonster();
+    fighting === 2 ? winGame() : defeatMonster()
+  }
+  if (Math.random() <= .1 && inventory.length !== 1){
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
   }
 }
+
+
+
+function getMonsterAttackValue(level){
+  let hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return hit;
+}
+
+function isMonsterHit(){
+  return Math.random() > .2 || health < 20;
+
+}
+
 function dodge(){
   text.innerText = "You dodged the attack from the " + monsters[fighting].name + ".";
 } 
@@ -247,4 +281,8 @@ function restart(){
   healthText.innerText = health;
   xpText.innerText = xp;
   goTown();
+}
+
+function winGame(){
+  update(locations[6]);
 }
